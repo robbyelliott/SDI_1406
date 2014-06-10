2f6c72fca0f7caddec75b737b0fc61e1df3872d9
Robby Elliott <robbyelliott@Robbys-MacBook-Pro.local>
2014-06-10 16:35:51 -0400

    Adjusted the js artifact to corrent name.  Modified html to reference rename.

---
+++ Elliott_Robert_assignment2.js
@@ -0,0 +1,26 @@
+//alert("JavaScript works!");
+
+/* 
+Robert Elliott
+June 10, 2014
+Project 2
+Undetermined
+*/
+
+
+//Vars
+
+
+
+
+
+//Function
+
+
+
+
+//Returned Values
+
+
+
+//Function Calls
