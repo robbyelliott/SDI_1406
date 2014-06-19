52df3a8ef2fdf82d0116f64abfe46b5d99e843cf
Robby Elliott <robbyelliott@robbys-mbp.home>
2014-06-19 19:24:43 -0400

    Adding project 3 folder work.

---
+++ sdi-project3/Elliott_Robert_Project3.js
@@ -0,0 +1,33 @@
+//alert("JavaScript works!");
+
+/* 
+Robert Elliott
+SDI - 1406
+Project 3
+*/
+
+// Global Variables
+var allset;
+
+
+
+
+//Functions
+var nephewData = function (json2){
+    console.log (json2);
+    
+};
+
+
+
+//Main Section of code
+
+allset = confirm ("Ready to take care of the yard?")
+    if (allset === true) {
+        console.log ("Great, lets get started.");
+    } else {
+        console.log ("Sorry, this isn't optional.");
+        
+    };
+    
+
---
+++ sdi-project3/json.js
@@ -0,0 +1,28 @@
+//JSON Information
+//Robert Elliot
+//SDI 1406
+//Project 3
+
+var json2 = {
+    "Nephews": [
+        {
+            "Name": "Todd",
+            "Age" : 22,
+            "Mow Time": 45,
+            "Edging Time": 25
+        },
+         {
+            "Name" : "Bobby",
+            "Age" : 18,
+            "Mow Time": 60,
+            "Edging Time": 30              
+        },
+        
+         {
+            "Name" : "James",
+            "Age" : 11,
+            "Mow Time": 90,
+            "Edging Time": 45   
+        }
+]
+};
