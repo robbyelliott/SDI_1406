d852fbbc2115908e48497df756e61acc77f8ad0b
Robby Elliott <robbyelliott@robbys-mbp.home>
2014-06-19 22:15:39 -0400

    modified syntax on the js and ccorrecting old issue.

--- sdi-project3/Elliott_Robert_Project3.js
+++ sdi-project3/Elliott_Robert_Project3.js
@@ -7,39 +7,69 @@ Project 3
 */
 
 // Global Variables
-var daysNeeded;
+
+var promptValue;
 var numHrs = 8;
 
-
-//Function Junction
-//Number of Days to hours
-mathHours = function(daysNeeded,numHrs){
-    totalHours = (daysNeeded * numHrs);
-      if (totalHours <= 40) {
-        console.log ("Equal or less than 40 hours.");
-      } else {
-        console.log ("Not less than 40 hours.");
-        
-      };
-    
-    console.log (totalHours);
+//Boolean Function
+var booInfo = function (daysNeeded, daysConfirm) {
+	while (daysNeeded > 5) 
+		daysNeeded --
+		console.log (+daysNeeded)			
+			if (daysNeeded < 5) {
+			console.log ("You shall pass!");
+			} else {
+			console.log ("You shall not pass!");
+			}
+	return daysNeeded
 };
 
-//Confirm the hours
-var hrsConfirm = function (totalHours){
-  while (totalHours < 40) 
-    console.log ("Hours exceed the limit.");
-    totalHours++;
-        if (totalHours > 40) {
-            console.log ("Hours have been adjusted.");
-        }
-    return totalHours;
+//Number Function
+var numFunction = function (numHrs) {
+	for (numHrs; numHrs < 40; numHrs++)
+		if (numHrs = 40) {
+		console.log ("Less than " +numHrs);
+		} else {
+		console.log ("Not less than " +numHrs);
+		}
+	console.log ("Number of " +numHrs);
+	return numHrs;
+};
+
+//String Function
+var stringFun = function (textDays, textHours) {
+        var explText = ("You have requested " +textDays+ " days, which will take " +textHours+ " hours.");
+	console.log (explText);
+	return explText;
+
 };
 
 
-// **Main Code**
-//Prompt for number of days needed.
-daysNeeded = prompt ("How many days are needed?", "Value between 1 and 10");
-mathHours (daysNeeded,numHrs);
-hrsConfirm (mathHours)
-console.log (totalHours)
+//***MAIN SECTION**
+	// prompt section
+
+promptValue = prompt ("How many days are needed?", "Value between 1 and 10");
+
+	//procedure
+var daysNeeded = parseInt(promptValue);
+ if (promptValue < 5) {
+ 	console.log ("Great we can handle " +promptValue+ " days.");
+ } else {
+ 	console.log ("We may have to evaluate " +promptValue+ " days.");
+ 
+ };  
+	
+	//Confirm
+daysConfirm = confirm ("Number of days requested was " +promptValue+ "?");
+	
+	//function calls
+var booReturn = booInfo (daysNeeded);
+var numReturn = numFunction(numHrs);
+var stringReturn = stringFun (daysNeeded, numHrs);
+
+//Returned Values
+console.log ("Returned value for: " +promptValue);
+console.log ("Returned value for: " +numReturn);
+console.log ("Returned value for: " +daysNeeded);
+console.log ("Returned value for: " +stringReturn);
+
