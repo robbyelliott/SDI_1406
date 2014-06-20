2e9f72768adaabf96c32efadd5b43ccca0830c3b
Robby Elliott <robbyelliott@robbys-mbp.home>
2014-06-19 21:17:52 -0400

    scrapped what I was doing and pulled code from project 2.

--- sdi-project3/Elliott_Robert_Project3.js
+++ sdi-project3/Elliott_Robert_Project3.js
@@ -7,27 +7,39 @@ Project 3
 */
 
 // Global Variables
-var allset;
+var daysNeeded;
+var numHrs = 8;
 
 
-
-
-//Functions
-var nephewData = function (json2){
-    console.log (json2);
+//Function Junction
+//Number of Days to hours
+mathHours = function(daysNeeded,numHrs){
+    totalHours = (daysNeeded * numHrs);
+      if (totalHours <= 40) {
+        console.log ("Equal or less than 40 hours.");
+      } else {
+        console.log ("Not less than 40 hours.");
+        
+      };
     
+    console.log (totalHours);
+};
+
+//Confirm the hours
+var hrsConfirm = function (totalHours){
+  while (totalHours < 40) 
+    console.log ("Hours exceed the limit.");
+    totalHours++;
+        if (totalHours > 40) {
+            console.log ("Hours have been adjusted.");
+        }
+    return totalHours;
 };
 
 
-
-//Main Section of code
-
-allset = confirm ("Ready to take care of the yard?")
-    if (allset === true) {
-        console.log ("Great, lets get started.");
-    } else {
-        console.log ("Sorry, this isn't optional.");
-        
-    };
-    
-
+// **Main Code**
+//Prompt for number of days needed.
+daysNeeded = prompt ("How many days are needed?", "Value between 1 and 10");
+mathHours (daysNeeded,numHrs);
+hrsConfirm (mathHours)
+console.log (totalHours)
--- sdi-project3/json.js
+++ sdi-project3/json.js
@@ -7,20 +7,17 @@ var json2 = {
     "Nephews": [
         {
             "Name": "Todd",
-            "Age" : 22,
             "Mow Time": 45,
             "Edging Time": 25
         },
          {
             "Name" : "Bobby",
-            "Age" : 18,
             "Mow Time": 60,
             "Edging Time": 30              
         },
         
          {
             "Name" : "James",
-            "Age" : 11,
             "Mow Time": 90,
             "Edging Time": 45   
         }
