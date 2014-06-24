47d4e2accac845ccd2c03b4c8272feb6b7db4d4f
Robby Elliott <robbyelliott@robbys-mbp.home>
2014-06-23 21:06:53 -0400

    Added scenario 2 information and changed input on scenario1.

--- sdi-project4/Elliott_Robert_assignment4.js
+++ sdi-project4/Elliott_Robert_assignment4.js
@@ -4,22 +4,36 @@
 	Assignment 4
 */
 var promptNumReturn;
+var phFuncReturn;
+var promptEmailReturn;
 
 
 //String Function
-	//Scenario 1: Does a string follow a 123-456-7890
+	//Scenario 1: Does a string follow a 123-456-7890?
             var phNumValidate = function (promptNumReturn) {
             var numLength = (promptNumReturn.length);
             var hyphenIndex = (promptNumReturn.indexOf ("-"));
             var hyphenLast = (promptNumReturn.lastIndexOf ("-"));
                 if (numLength == 12 && hyphenIndex == 3 && hyphenLast == 7) {
-                    return true
+                    return phFuncReturn;
                 };
             };
+
+        //Scenario 2: Does a string follow an aaa@bbb.ccc pattern like an email address?
+            var validateEmail = function (emailValue) {
                 
+                
+            
+            
+            };
+            
+        
+
 //Main Code
 	//Scenario 1: String for phone number
-	    promptNumReturn = prompt ("Enter your telephone number for validation","000-000-0000");
+	    promptNumReturn = ("123-456-7890");
             console.log (promptNumReturn);
-            phNumValidate (promptNumReturn)
+            phNumValidate (promptNumReturn);
             
+	//Scenario 2: String for email address
+            emailValueSend = ("robbyelliott@fullsail.edu");
