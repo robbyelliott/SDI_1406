e96c6efacc40fd13504b69c9c25599c9575d8aee
Robby Elliott <robbyelliott@robbys-mbp.home>
2014-06-24 18:11:32 -0400

    Working on the title case scenario.

--- sdi-project4/Elliott_Robert_assignment4.js
+++ sdi-project4/Elliott_Robert_assignment4.js
@@ -5,35 +5,32 @@
 */
 var promptNumReturn;
 var phFuncReturn;
-var promptEmailReturn;
 
 
 //String Function
-	//Scenario 1: Does a string follow a 123-456-7890?
+	//Scenario 1: Does a string follow a pattern?
             var phNumValidate = function (promptNumReturn) {
             var numLength = (promptNumReturn.length);
             var hyphenIndex = (promptNumReturn.indexOf ("-"));
             var hyphenLast = (promptNumReturn.lastIndexOf ("-"));
                 if (numLength == 12 && hyphenIndex == 3 && hyphenLast == 7) {
-                    return phFuncReturn;
+                    console.log ("True");
                 };
             };
-
-        //Scenario 2: Does a string follow an aaa@bbb.ccc pattern like an email address?
-            var validateEmail = function (emailValue) {
-                
-                
-            
-            
+        
+        //Scenario 4: Title-Case a string
+            var splitString = function (val){
+              var sepString = val.split(" ")  
+                console.log (sepString)
             };
-            
         
 
 //Main Code
 	//Scenario 1: String for phone number
-	    promptNumReturn = ("123-456-7890");
+	    promptNumReturn = ("123-456-7000");
             console.log (promptNumReturn);
             phNumValidate (promptNumReturn);
             
-	//Scenario 2: String for email address
-            emailValueSend = ("robbyelliott@fullsail.edu");
+	//Scenario 4: String for title-case
+            var stringTitle = splitString("i think lines mostly filler.");
+            
